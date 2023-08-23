0e9f68db061d594beaa422f1c1414abb87998b3b
opoberezh <opoberezh.2015@gmail.com>
2023-08-23 22:37:04 +0200

    huks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,102 +1,66 @@
-import { Component } from "react";
+
 import { nanoid } from "nanoid";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import initialContacts from "./contactsList.json";
 import { Layout } from "./Layout/Layout";
 import { GlobalStyle } from "./GlobalStyled";
 import { WrapperContainer } from "./App.styled";
+import { useState } from "react";
 
-export class App extends Component {
-  state ={
-    contacts: initialContacts,
-    initialContacts: initialContacts,
-    contactFilter: '',
-  };
+export const App = () => {
+ const [contacts, setContacts] = useState (initialContacts);
+const [contactFilter, setContactFilter] = useState('')
+};
 
-componentDidMount(){
-  console.log('componentDidMount');
-const savedContacts = localStorage.getItem('contacts');
-if(savedContacts !== null){
- this.setState({
-  contacts: JSON.parse(savedContacts)
- });
- 
+
+const resetChanges = () =>{
+  setContacts(initialContacts);
 }
+
+const addContact = newContact => {
+  const existingContact = contacts.find(
+    contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+  );
   
-};
-
-componentDidUpdate(prevProps, prevState){
-  console.log('this.state:', this.state);
-  console.log('prevState:', prevState)
-  if (this.state.contacts !== prevState.contacts){
-  console.log(`the contacts are updated`);
-  localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  if (existingContact) {
+    toast.info(`${newContact.name} is already in contacts.`, {
+      position: "top-right",
+      autoClose: 2000,});
+    return;
   }
+  const newItem = {
+    id: nanoid(),
+    name: newContact.name,
+    number: newContact.number,
+  };
+
+  setContacts(prevState => ({
+    ...prevState, 
+    newItem
+  }));
 };
+ 
+  const visibleContactItems = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(contactFilter.toLowerCase())
+  );
 
-resetChanges = ()=>{
-  this.setState({
-    contacts: this.state.initialContacts,
-  })
-}
-// componentWillUnmount(){
-
-// };
-
-  addContact = newContact => {
-    const existingContact = this.state.contacts.find(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    );
-
-    if (existingContact) {
-      alert(`${newContact.name} is already in contacts.`);
-      return;
-    }
-    const newItem = {
-      id: nanoid(),
-      name: newContact.name,
-      number: newContact.number,
-    };
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newItem],
-      initialContacts: [...prevState.initialContacts, newItem],
-    }));
-  };
-
-  changeContactFilter = newFilter => {
-    this.setState({
-      contactFilter: newFilter,
-    });
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  render() {
-    const { contacts, contactFilter } = this.state;
-    const visibleContactItems = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(contactFilter.toLowerCase())
-    );
-    return (
-      <Layout>
-        <WrapperContainer>
-        <ContactForm onAdd={this.addContact} />
-        <ContactList
-          contacts={visibleContactItems}
-          contactFilter={contactFilter}
-          onChangeFilter={this.changeContactFilter}
-          onDeleteContact={this.deleteContact}
-          onReset={this.resetChanges}
-        />
-        </WrapperContainer>
-        <GlobalStyle/>
-      </Layout>
-    );
-  };
-};
-
+  return (
+    <Layout>
+      <WrapperContainer>
+      <ContactForm onAdd={addContact} />
+      <ContactList
+        contacts={visibleContactItems}
+        contactFilter={contactFilter}
+        onChangeFilter={this.changeContactFilter}
+        onDeleteContact={this.deleteContact}
+        onReset={resetChanges}
+      />
+      <ToastContainer />
+      </WrapperContainer>
+      <GlobalStyle/>
+    </Layout>
+  );
